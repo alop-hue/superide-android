a2140e6c29a817d0714dedd94c9592654f99bd8b
ATHUL A S <athulas2005@gmail.com>
2025-04-24 23:23:51 +0530

    optimization 1

--- lib/utils/widgets.dart
+++ lib/utils/widgets.dart
@@ -115,9 +115,11 @@ class CodeEditor extends StatefulWidget {
   final String? file;
   final File filePath;
   final bool isTemplate;
-  const CodeEditor(
-    {super.key,
+  final CodeController codeController;
+  const CodeEditor({
+    super.key,
     required this.language,
+    required this.codeController,
     this.theme,
     this.file,
     required this.filePath,
@@ -130,93 +132,60 @@ class CodeEditor extends StatefulWidget {
 }
 
 class _CodeEditorState extends State<CodeEditor> {
-  late  CodeController codeController;
   double _initialFontSize = 10.0;
   double _currentScale = 1.0;
 
-  Future<String?> getData() async {
-    if (widget.filePath.existsSync()) {
-      String source = await widget.filePath.readAsString();
-      if (source.isNotEmpty) {
-        return source;
-      } else {
-        if(widget.isTemplate) {
-          return widget.language.helloWorld;
-        }else{
-          return 'Your canvas is ready.\nWrite something amazing!';
-        }
-      }
-    }
-    return widget.language.helloWorld;
-  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<FindWordBloc, FindWordState>(
-      builder: (context, wordState) {
-        return FutureBuilder(
-          future: getData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            codeController = CodeController(
-              language: widget.language.language,
-              text: snapshot.hasData? (snapshot.data??widget.language.helloWorld) : "Can't read filecontent",
-              patternMap: {
-                if (wordState.word.isNotEmpty) RegExp.escape(wordState.word): TextStyle(backgroundColor: Colors.yellow[600]!)
-              });
-            return BlocBuilder<ThemeBloc, ThemeState>(
-              builder: (context, state) {
-                return CodeTheme(
-                  data: CodeThemeData(styles: highlightThemes[state.theme]),
-                  child: GestureDetector(
-                    onScaleStart: (details) {
-                      if(details.pointerCount == 2){
-                        _initialFontSize = state.fontSize;
-                      }
-                    },
-                    onScaleUpdate: (details) {
-                      if (details.pointerCount == 2) {
-                        _currentScale = details.scale;
-                        double newFontSize = _initialFontSize * _currentScale;
-                        newFontSize = newFontSize.clamp(8.0, 48.0);
-                        context.read<ThemeBloc>().add(SetFontSize(fontSize: newFontSize));
-                      }
-                    },
-                    child: BlocBuilder<CursorMovementBloc, CursorMovementState>(
-                      builder: (context, offsetState) {
-                        WidgetsBinding.instance.addPostFrameCallback((_) {
-                          if (offsetState.offset >= 0 && offsetState.offset <= codeController.text.length) {
-                            codeController.selection = TextSelection.collapsed(offset: offsetState.offset);
-                          }
-                        });
-                        Timer? debounce;
-                        return CodeField(
-                          onTap: () => editorOffset = codeController.selection.baseOffset,
-                          onChanged: (word) {
-                            if (debounce?.isActive ?? false) debounce!.cancel();
-                            debounce = Timer(const Duration(milliseconds: 500), () {
-                              widget.filePath.writeAsString(word);
-                            });
-                          },
-                          textStyle: TextStyle(fontFamily: state.fontFamily, fontSize: state.fontSize),
-                          textSelectionTheme: const TextSelectionThemeData(
-                            cursorColor: Color(0xff23a9f2),
-                            selectionColor:Color.fromARGB(112, 30, 134, 245)
-                          ),
-                          controller: codeController,
-                          expands: true,
-                          maxLines: null,
-                          minLines: null,
-                        );
-                      },
-                    ),
+    final CodeController codeController = widget.codeController;
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (context, state) {
+        return CodeTheme(
+          data: CodeThemeData(styles: highlightThemes[state.theme]),
+          child: GestureDetector(
+            onScaleStart: (details) {
+              if(details.pointerCount == 2){
+                _initialFontSize = state.fontSize;
+              }
+            },
+            onScaleUpdate: (details) {
+              if (details.pointerCount == 2) {
+                _currentScale = details.scale;
+                double newFontSize = _initialFontSize * _currentScale;
+                newFontSize = newFontSize.clamp(8.0, 48.0);
+                context.read<ThemeBloc>().add(SetFontSize(fontSize: newFontSize));
+              }
+            },
+            child: BlocBuilder<CursorMovementBloc, CursorMovementState>(
+              builder: (context, offsetState) {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  if (offsetState.offset >= 0 && offsetState.offset <= codeController.text.length) {
+                    codeController.selection = TextSelection.collapsed(offset: offsetState.offset);
+                  }
+                });
+                Timer? debounce;
+                return CodeField(
+                  onTap: () => editorOffset = codeController.selection.baseOffset,
+                  onChanged: (word) {
+                    if (debounce?.isActive ?? false) debounce!.cancel();
+                    debounce = Timer(const Duration(milliseconds: 500), () {
+                      widget.filePath.writeAsString(word);
+                    });
+                  },
+                  textStyle: TextStyle(fontFamily: state.fontFamily, fontSize: state.fontSize),
+                  textSelectionTheme: const TextSelectionThemeData(
+                    cursorColor: Color(0xff23a9f2),
+                    selectionColor:Color.fromARGB(112, 30, 134, 245)
                   ),
+                  controller: codeController,
+                  expands: true,
+                  maxLines: null,
+                  minLines: null,
                 );
               },
-            );
-          }
+            ),
+          ),
         );
       },
     );
